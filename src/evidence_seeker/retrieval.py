bf58ca693d604afa11d1bb627bb864caaed0a67c
Gregor Betz <3662782+ggbetz@users.noreply.github.com>
2024-11-25 17:58:58 +0100

    docu

--- src/evidence_seeker/retrieval.py
+++ src/evidence_seeker/retrieval.py
@@ -5,10 +5,12 @@ from __future__ import annotations
 
 class DocumentRetriever:
     async def retrieve_documents(self, claim: dict) -> list[dict]:
-        return [
+        """retrieve all documents that are relevant for the claim and/or its negation"""
+        dummy_documents = [
             {"text": "document1", "uid": "001"},
             {"text": "document2", "uid": "002"},
         ]
+        return dummy_documents  
 
     async def __call__(self, claim: dict) -> dict:
         claim["documents"] = await self.retrieve_documents(claim)
