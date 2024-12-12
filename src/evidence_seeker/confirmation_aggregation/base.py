3a4a058eda729318822f7b9d87ee957207054b28
Gregor Betz <3662782+ggbetz@users.noreply.github.com>
2024-12-12 16:40:28 +0100

    add config

--- src/evidence_seeker/confirmation_aggregation/base.py
+++ src/evidence_seeker/confirmation_aggregation/base.py
@@ -1,38 +1,58 @@
 "confirmation_aggregation"
 
-
 import numpy as np
+import pathlib
+import pydantic
+import yaml
 
 from evidence_seeker.datamodels import CheckedClaim
 
 
-_CONFIRMATION_THRESHOLD = 0.2
+class ConfirmationAggregationConfig(pydantic.BaseModel):
+    confirmation_threshold: float = 0.2
 
 
 class ConfirmationAggregator:
+    def __init__(self, config: ConfirmationAggregationConfig | None = None):
+        if config is None:
+            config = ConfirmationAggregationConfig()
+        self.config = config
+
     async def verbalize_confirmation(self, claim: CheckedClaim) -> str:
-        if claim.average_confirmation > .6:
+        if claim.average_confirmation > 0.6:
             return "The claim is strongly confirmed."
-        if claim.average_confirmation > .4:
+        if claim.average_confirmation > 0.4:
             return "The claim is confirmed."
-        if claim.average_confirmation > .2:
+        if claim.average_confirmation > 0.2:
             return "The claim is weakly confirmed."
-        if claim.average_confirmation < -.6:
+        if claim.average_confirmation < -0.6:
             return "The claim is strongly disconfirmed."
-        if claim.average_confirmation < -.4:
+        if claim.average_confirmation < -0.4:
             return "The claim is disconfirmed."
-        if claim.average_confirmation < -.2:
+        if claim.average_confirmation < -0.2:
             return "The claim is weakly disconfirmed."
         return "The claim is neither confirmed nor disconfirmed."
 
     async def __call__(self, claim: CheckedClaim) -> CheckedClaim:
-        claim.n_evidence = len([c for c in claim.confirmation_by_document.values() if abs(c) > _CONFIRMATION_THRESHOLD])
-        claim.average_confirmation = sum(
-            claim.confirmation_by_document.values()
-        ) / len(claim.confirmation_by_document)
+        relevant_conf_by_docs = claim.confirmation_by_document or {}
+        relevant_conf_by_docs = {
+            k: c
+            for k, c in relevant_conf_by_docs.items()
+            if abs(c) > self.config.confirmation_threshold
+        }
+        claim.n_evidence = len(relevant_conf_by_docs)
+        claim.average_confirmation = float(
+            np.mean(list(relevant_conf_by_docs.values()))
+        )
         claim.evidential_uncertainty = float(
-            np.var(list(claim.confirmation_by_document.values()))
+            np.var(list(relevant_conf_by_docs.values()))
         )
         claim.verbalized_confirmation = await self.verbalize_confirmation(claim)
 
         return claim
+
+    @staticmethod
+    def from_config_file(config_file: str):
+        path = pathlib.Path(config_file)
+        config = ConfirmationAggregationConfig(**yaml.safe_load(path.read_text()))
+        return ConfirmationAggregator(config=config)
