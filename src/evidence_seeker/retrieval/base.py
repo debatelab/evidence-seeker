85ec23dcf1213fa762b152e355c8b8ea2fe00495
leoniew <wahl.leonie@web.de>
2024-12-14 23:59:18 +0100

    Add optional local storage of downloaded index

--- src/evidence_seeker/retrieval/base.py
+++ src/evidence_seeker/retrieval/base.py
@@ -78,21 +78,23 @@ class DocumentRetriever:
                     f"Index persist path {self.index_persist_path} not found."
                 )
 
-        if not self.index_persist_path:
+        if self.index_hub_path:
             logger.info(f"Downloading index from hub at {self.index_hub_path}")
             import huggingface_hub
 
             HfApi = huggingface_hub.HfApi(token=self.token)
-            persist_dir = tempfile.mkdtemp()
+            if not self.index_persist_path:
+                persist_dir = tempfile.mkdtemp()
             HfApi.snapshot_download(
                 repo_id=self.index_hub_path,
                 repo_type="dataset",
                 local_dir=persist_dir,
                 token=self.token,
             )
-            persist_dir = os.path.join(persist_dir, "index")
+            if not self.index_persist_path or os.path.basename(persist_dir.removesuffix('/')) != 'index':
+                persist_dir = os.path.join(persist_dir, "index")
+                logger.info(f"Setting index directory to {persist_dir}")
 
-        logger.info(f"Loading index from disk at {persist_dir}")
         # rebuild storage context
         storage_context = StorageContext.from_defaults(persist_dir=persist_dir)
         # load index
