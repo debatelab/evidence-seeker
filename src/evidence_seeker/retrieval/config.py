32c20a7b2e653834b2053656d2bc6990ae43d415
Sebastian Cacean <xylomorpheus@googlemail.com>
2025-06-17 11:03:55 +0200

    Refactoring config and adding validation for input.

--- src/evidence_seeker/retrieval/config.py
+++ src/evidence_seeker/retrieval/config.py
@@ -1,8 +1,23 @@
 "retrieval config"
 
 import pydantic
+from pydantic import model_validator
+from loguru import logger
+import enum
 
 from evidence_seeker.datamodels import StatementType
+
+
+class EmbedBackendType(enum.Enum):
+    # Embedding via TEI (e.g., as provided by HuggingFace as a service)
+    TEI = "tei"
+    # Local embedding via ollama
+    # TODO/TOFIX: Ollama embedding throws errors. Check if we can fix it.
+    OLLAMA = "ollama"
+    # Local embedding via huggingface
+    HUGGINGFACE = "huggingface"
+    # HF Inference API
+    HUGGINGFACE_INFERENCE_API = "huggingface_inference_api"
 
 
 class RetrievalConfig(pydantic.BaseModel):
@@ -24,3 +39,87 @@ class RetrievalConfig(pydantic.BaseModel):
     index_hub_path: str | None = None
     top_k: int = 8
     ignore_statement_types: list[str] = [StatementType.NORMATIVE.value]
+
+    @model_validator(mode='after')
+    def check_base_url(
+        cls,
+        config: 'RetrievalConfig'
+    ) -> 'RetrievalConfig':
+        if (
+            not config.embed_base_url
+            and (
+                config.embed_backend_type == EmbedBackendType.TEI.value
+                or (
+                    config.embed_backend_type
+                    == EmbedBackendType.HUGGINGFACE_INFERENCE_API.value
+                )
+            )
+        ):
+            msg = (
+                "'embed_base_url' must be set for the selected "
+                "embed_backend_type. Please provide a valid URL."
+            )
+            logger.error(msg)
+            raise ValueError(msg)
+
+        return config
+
+    @model_validator(mode='after')
+    def check_api_token_name(
+        cls,
+        config: 'RetrievalConfig'
+    ) -> 'RetrievalConfig':
+        if (
+            not config.api_key_name
+            and (
+                config.embed_backend_type == EmbedBackendType.TEI.value
+                or (
+                    config.embed_backend_type
+                    == EmbedBackendType.HUGGINGFACE_INFERENCE_API.value
+                )
+            )
+        ):
+            msg = (
+                f"Check whether you need an API token for your backend "
+                f"('{config.embed_backend_type}'). If you need one, set an "
+                "`api_key_name` in the retriever config and provide the "
+                " api token as an environment variable with that name."
+            )
+            logger.warning(msg)
+        return config
+
+    @model_validator(mode='after')
+    def check_hub_token_name(
+        cls,
+        config: 'RetrievalConfig'
+    ) -> 'RetrievalConfig':
+        if (
+            not config.hub_key_name
+            and not config.index_hub_path
+        ):
+            msg = (
+                "Check whether you need a HF hub token for saving/loading "
+                "your index to/from the Hugging Face Hub. "
+                "If you need one, set an "
+                "`hub_key_name` in the retriever config and provide the "
+                " token as an environment variable with that name."
+            )
+            logger.warning(msg)
+        return config
+
+    @model_validator(mode='after')
+    def check_index_path(
+        cls,
+        config: 'RetrievalConfig'
+    ) -> 'RetrievalConfig':
+        if (
+            not config.index_persist_path
+            and not config.index_hub_path
+        ):
+            err_msg = (
+                "Either 'index_persist_path' or 'index_hub_path' must "
+                "be provided to store/load the index."
+            )
+            logger.error(err_msg)
+            raise ValueError(err_msg)
+        return config
