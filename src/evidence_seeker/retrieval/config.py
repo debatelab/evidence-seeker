b8d350a5c91c68a2b7d0987cdcca93027d1806ee
Sebastian Cacean <xylomorpheus@googlemail.com>
2025-06-17 09:28:18 +0200

    Update default retriever config.

--- src/evidence_seeker/retrieval/config.py
+++ src/evidence_seeker/retrieval/config.py
@@ -8,19 +8,19 @@ from evidence_seeker.datamodels import StatementType
 class RetrievalConfig(pydantic.BaseModel):
     # TODO: Add field Descriptions about embed_backend_type
     config_version: str = "v0.1"
-    description: str = "Erste Version einer Konfiguration für den Retriever der EvidenceSeeker Boilerplate."
+    description: str = "Configuration of EvidenceSeeker's retriever component."
     embed_base_url: str | None = None
-    embed_model_name: str | None = "sentence-transformers/paraphrase-multilingual-mpnet-base-v2"
+    embed_model_name: str = "sentence-transformers/paraphrase-multilingual-mpnet-base-v2"
     embed_backend_type: str = "huggingface"
     bill_to: str | None = None
-    api_key_name: str | None = None
-    hub_key_name: str | None = None
+    api_key_name: str | None = "API_TOKEN_EMBEDDING_MODEL"
+    hub_key_name: str | None = "HF_HUB_TOKEN"
     embed_batch_size: int = 32
-    document_input_dir: str | None = None
+    document_input_dir: str | None = "./knowledge_base/data_files"
     document_input_files: list[str] | None = None
     window_size: int = 3
     index_id: str = "default_index_id"
-    index_persist_path: str | None = None
+    index_persist_path: str | None = "./embeddings"
     index_hub_path: str | None = None
     top_k: int = 8
     ignore_statement_types: list[str] = [StatementType.NORMATIVE.value]
