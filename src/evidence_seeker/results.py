43667366efd1441c952e7de0be4f71069fea8fb4
Sebastian Cacean <sebastian.cacean@gmail.com>
2025-07-18 12:33:06 +0200

    Slight reformatting.

--- src/evidence_seeker/results.py
+++ src/evidence_seeker/results.py
@@ -5,16 +5,25 @@ import pydantic
 
 from evidence_seeker.preprocessing.config import ClaimPreprocessingConfig
 from evidence_seeker.retrieval.config import RetrievalConfig
-from evidence_seeker.confirmation_analysis.config import ConfirmationAnalyzerConfig
+from evidence_seeker.confirmation_analysis.config import (
+    ConfirmationAnalyzerConfig
+)
+
 
 class EvidenceSeekerResult(pydantic.BaseModel):
-    request_uid : str | None = None
-    request : str | None = None
-    request_time : str | None = None
-    retrieval_config : RetrievalConfig | None = RetrievalConfig(env_file=None)
-    confirmation_config : ConfirmationAnalyzerConfig | None = ConfirmationAnalyzerConfig()
-    preprocessing_config : ClaimPreprocessingConfig | None = ClaimPreprocessingConfig()
-    claims : list[dict] = []
-    feedback : dict[str, Any] = {
-        "binary" : None
+
+    request_uid: str | None = None
+    request: str | None = None
+    request_time: str | None = None
+    retrieval_config: RetrievalConfig | None = RetrievalConfig(env_file=None)
+    confirmation_config: ConfirmationAnalyzerConfig | None = (
+        ConfirmationAnalyzerConfig()
+    )
+    preprocessing_config: ClaimPreprocessingConfig | None = (
+        ClaimPreprocessingConfig()
+    )
+    claims: list[dict] = []
+    feedback: dict[str, Any] = {
+        # TODO: perhaps better with an enum.Enum?
+        "binary": None
     }
